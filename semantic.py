fbeedaa86d25be95d9376fc3ee73631401935396
Merlin9341 <felixmlroughton@gmail.com>
2023-02-13 16:01:35 +0000

    Note on using en_core_web_sm with example.py

--- semantic.py
+++ semantic.py
@@ -40,3 +40,12 @@ from tabulate import tabulate
 #list for table made using list comprehension
 similarity_table = [[str(token1)] + [str(token1.similarity(token2)) for token2 in new_tokens] for token1 in new_tokens]
 print(tabulate(similarity_table, headers= ["Word"] + [token_title for token_title in new_tokens], tablefmt="grid"))
+
+#When the example fule is run with the model "en_core_web_sm" the following error is returned
+#UserWarning: [W007] 
+#The model you're using has no word vectors loaded, so the result of the Doc.similarity method will be based on the tagger,
+#parser and NER, which may not give useful similarity judgements. This may happen if you're using one of the small models, 
+#e.g. `en_core_web_sm`, which don't ship with word vectors and only use context-sensitive tensors. You can always add your own word vectors,
+#or use one of the larger models instead if available
+#
+#This model does not include vectors and so cannot be used to work out similarity between words
